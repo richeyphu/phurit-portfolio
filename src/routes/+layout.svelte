d4cff0dd4fb1f9f6c6f06a33efe84b9ed772831d
AkaraSellegg <55230837+richeyphu@users.noreply.github.com>
2023-07-06 05:01:18 +0700

    Add footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
-<script>
+<script lang="ts">
 	import Header from './Header.svelte';
+	import Footer from './Footer.svelte';
 	import '../app.css';
 	import './styles.css';
 </script>
@@ -11,11 +12,7 @@
 		<slot />
 	</main>
 
-	<!-- 
-	<footer>
-		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
-	</footer>
-	-->
+	<Footer />
 </div>
 
 <style>
@@ -35,23 +32,5 @@
 		margin: 0 auto;
 		box-sizing: border-box;
 	}
-	
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 12px;
-	}
-
-	footer a {
-		font-weight: bold;
-	}
-
-	@media (min-width: 480px) {
-		footer {
-			padding: 12px 0;
-		}
-	} 
 	*/
 </style>
---
+++ src/routes/Footer.svelte
@@ -0,0 +1,19 @@
+<script>   
+	const year = new Date().getFullYear();
+</script>
+
+<footer>
+	<div class="absolute bottom-0 w-full p-2 flex justify-center">
+		<a class="font-extralight text-sm text-gray-300" href="/"> © {year} phurit.de </a>
+	</div>
+</footer>
+
+<style>
+	footer {
+		position: absolute;
+		width: 100%;
+		height: 50px;
+		background: linear-gradient(to top, rgba(0, 0, 0, 0.1), transparent);
+		bottom: 0;
+	}
+</style>
