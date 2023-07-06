4925d9a952ff22231acabde2e5b99941c9ae9564
AkaraSellegg <55230837+richeyphu@users.noreply.github.com>
2023-07-07 06:33:44 +0700

    Add current page indicator in navbar

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,28 +1,43 @@
 <script lang="ts">
 	import { page } from '$app/stores';
-	import logo from '$lib/images/svelte-logo.svg';
-	import github from '$lib/images/github.svg';
 </script>
 
 <header class="p-3">
 	<div class="flex justify-between">
-		<a href="/" class="w-10 brightness-50 invert">
+		<a href="/" class="w-10 brightness-50 invert hover:opacity-90 transition-all duration-200">
 			<img src="/favicon.png" alt="phurit.de" />
 		</a>
 
-		<nav class="flex flex-row gap-3 my-auto pr-2 text-gray-200 text-lg font-light drop-shadow">
-			<a href="/">/home</a>
-			<a href="/projects">/projects</a>
-			<a href="/about">/about</a>
+		<nav
+			class="flex flex-row gap-3 my-auto pr-2 text-gray-200 text-lg font-light drop-shadow [&>a:hover]:opacity-80 [&>a]:transition-all [&>a]:duration-200"
+		>
+			<a
+				href="/"
+				class={$page.url.pathname === '/'
+					? 'bg-gray-900 bg-opacity-40 backdrop-blur-md px-2 rounded-md'
+					: ''}>/home</a
+			>
+			<a
+				href="/projects"
+				class={$page.url.pathname === '/projects'
+					? 'bg-gray-900 bg-opacity-40 backdrop-blur-md px-2 rounded-md'
+					: ''}>/projects</a
+			>
+			<a
+				href="/about"
+				class={$page.url.pathname === '/about'
+					? 'bg-gray-900 bg-opacity-40 backdrop-blur-md px-2 rounded-md'
+					: ''}>/about</a
+			>
 		</nav>
 	</div>
 </header>
 
-<style>
+<style lang="">
 	header {
 		position: fixed;
 		width: 100%;
-		height: 100px;
+		height: 65px;
 		background: linear-gradient(to bottom, rgba(0, 0, 0, 0.3), transparent);
 		z-index: 100;
 	}
