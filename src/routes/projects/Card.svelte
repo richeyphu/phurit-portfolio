5c82100034e1db794a701cc393dcd54d38edd9ac
AkaraSellegg <55230837+richeyphu@users.noreply.github.com>
2023-07-07 04:58:56 +0700

    Change animate direction

--- src/routes/projects/Card.svelte
+++ src/routes/projects/Card.svelte
@@ -4,7 +4,7 @@
 	export let link: string = '#';
 </script>
 
-<div class="animate__animated animate__fadeInDown">
+<div class="animate__animated animate__fadeInUp">
 	<a
 		href={link}
 		target={link.startsWith('#') ? '' : '_blank'}
