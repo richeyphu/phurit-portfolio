1f54fff842806b96fc76ca50a2c808027e60b0ca
AkaraSellegg <55230837+richeyphu@users.noreply.github.com>
2023-07-07 05:17:26 +0700

    Add custom error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
+<section class="flex h-screen">
+	<div class="m-auto animate__animated animate__fadeIn">
+		<h1 class="font-light text-4xl drop-shadow">
+			{$page.status} | {$page.error?.message}
+		</h1>
+	</div>
+</section>
+
+<style>
+</style>
