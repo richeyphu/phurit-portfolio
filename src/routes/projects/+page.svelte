09c3769630919067c8dd83f8276104a5ef5a06ed
AkaraSellegg <55230837+richeyphu@users.noreply.github.com>
2023-07-07 04:42:52 +0700

    Fix reversed project list mutation

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -12,7 +12,7 @@
 	<div class="mx-auto justify-center">
 		<h1 class="font-light text-4xl drop-shadow">Projects</h1>
 		<div class="grid grid-cols-1 gap-4 mt-8 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-			{#each projects.reverse() as project}
+			{#each [...projects].reverse() as project}
 				<Card title={project.title} description={project.description} link={project.link} />
 			{/each}
 			<Card title="Coming soon" />
