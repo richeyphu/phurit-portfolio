6d9f075818c55a3109bec57009c08050f87992f6
AkaraSellegg <55230837+richeyphu@users.noreply.github.com>
2023-07-06 07:04:33 +0700

    Add fade in animation

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -4,7 +4,7 @@
 </svelte:head>
 
 <content class="flex h-screen">
-	<div class="m-auto">
+	<div class="m-auto animate__animated animate__fadeIn">
 		<h1 class="font-light text-4xl drop-shadow">Coming soon</h1>
 	</div>
 </content>
