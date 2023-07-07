4854350f9ea7ef68bb07b9a2a8522d8b622812d8
AkaraSellegg <55230837+richeyphu@users.noreply.github.com>
2023-07-08 06:41:05 +0700

    Add more projects

--- src/routes/projects/projects.ts
+++ src/routes/projects/projects.ts
@@ -6,6 +6,26 @@ export interface Project {
 
 export const projects: Project[] = [
 	{
+		title: 'noicesoup',
+		description: 'Selenium-based Google images scraper tool',
+		link: 'https://github.com/richeyphu/noicesoup'
+	},
+	{
+		title: 'pyurlfuzzer',
+		description: 'A simple website directory scanner',
+		link: 'https://github.com/richeyphu/pyurlfuzzer'
+	},
+	{
+		title: 'PakFlag',
+		description: 'A simple CTF flags generator',
+		link: 'https://github.com/TNI-Cybersec/PakFlag'
+	},
+	{
+		title: 'Certgen',
+		description: 'A simple mass certificate generator',
+		link: 'https://github.com/TNI-Cybersec/Certgen'
+	},
+	{
 		title: 'Slaviii',
 		description: 'Never let your boss hungry again 🐈',
 		link: 'https://github.com/richeyphu/Slaviii'
@@ -26,8 +46,33 @@ export const projects: Project[] = [
 		link: 'https://github.com/richeyphu/piery'
 	},
 	{
+		title: 'PowBigInt.js',
+		description: 'A simple utility library for calculating powers of big integers',
+		link: 'https://github.com/richeyphu/PowBigInt.js'
+	},
+	{
+		title: 'Comcraft',
+		description: 'RESTful API server for a computer store, built with Express.js and MongoDB',
+		link: 'https://github.com/richeyphu/express-comcraft'
+	},
+	{
+		title: 'express-now',
+		description: 'Get a quick current status overview of an Express server at a glance',
+		link: 'https://github.com/richeyphu/express-now'
+	},
+	{
+		title: 'WatchDoge',
+		description: 'IoT based Rickrollable intrusion detection system',
+		link: 'https://github.com/richeyphu/WatchDoge'
+	},
+	{
 		title: 'Get Motivated',
 		description: 'Stop hornie, be motivated. Chrome extension that will save you from 😭',
 		link: 'https://github.com/richeyphu/GetMotivated'
+	},
+	{
+		title: 'dechroma.js',
+		description: 'Chroma keying for canvas-manipulated video',
+		link: 'https://github.com/richeyphu/dechroma.js'
 	}
 ];
