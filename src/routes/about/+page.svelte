bc4f7ef45adc2251f5d652464b631cd0e330bb69
AkaraSellegg <55230837+richeyphu@users.noreply.github.com>
2023-07-11 06:23:59 +0700

    Update about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,3 +1,21 @@
+<script lang="ts">
+	import Typewriter from 'svelte-typewriter';
+
+	const words = [
+		'Full-stack Developer',
+		'Software Engineer',
+		'Research Assistant',
+		'Photographer',
+		'Designer',
+		'Gamer',
+		'Writer',
+		'Learner',
+		'Human',
+		'.......',
+		'who I am'
+	];
+</script>
+
 <svelte:head>
 	<title>phurit.de / about</title>
 	<meta name="description" content="About me" />
@@ -5,6 +23,22 @@
 
 <section class="flex h-screen">
 	<div class="m-auto animate__animated animate__fadeIn">
-		<h1 class="font-light text-4xl drop-shadow">Coming soon</h1>
+		<h1 class="font-light text-4xl drop-shadow">
+			<Typewriter
+				mode="loopOnce"
+				wordInterval={1500}
+				--cursor-width="0.2rem"
+				--cursor-color="rgba(79, 209, 197, 0.75)"
+			>
+				<span data-static>I am</span>
+				{#each words as word, i}
+					{#if i >= words.length - 2}
+						<span>{`${word}.`}</span>
+					{:else}
+						<span>{`a ${word}`}</span>
+					{/if}
+				{/each}
+			</Typewriter>
+		</h1>
 	</div>
 </section>
