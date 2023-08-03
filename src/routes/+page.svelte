14d78b9eb326db5717f6a74ccb11c480ed1239f3
AkaraSellegg <55230837+richeyphu@users.noreply.github.com>
2023-08-04 04:56:42 +0700

    Fetch hit counter api

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,14 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
 	import Window from './Window.svelte';
+
+	onMount(() => {
+		// Fetch hit counter
+		fetch('https://counter.phuritd.workers.dev/hit/phurit.de/visits')
+			.then((res) => res.json())
+			.then((data) => console.log(data))
+			.catch((err) => console.log(err));
+	});
 </script>
 
 <svelte:head>
